2e371ff274f83f49218600a6d6aa4506343c2c10
Emily McLean <emilymcleandeveloper@gmail.com>
2025-05-18 11:40:54 +1000

    Add terraform to workflow

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,10 @@
-# resource "aws_s3_object" "lambda_function_object" {
-#   bucket = data.terraform_remote_state.main_infra.outputs.bucket_lambda_arn
+resource "aws_s3_object" "lambda_function_object" {
+  bucket  = data.terraform_remote_state.main_infra.outputs.bucket_lambda_arn
+  key     = "lambda-gtfsrt.zip"
+  content = data.local_file.lambda_function_archive.content
+  etag    = data.local_file.lambda_function_archive.content_md5
+}
 
-#   key    = "gtfsrt.zip"
-#   source = data.archive_file.lambda_function_archive.output_path
-
-#   etag = data.archive_file.lambda_function_archive.output_md5
-# }
+data "local_file" "lambda_function_archive" {
+  filename = "${path.module}/lambda-gtfsrt.zip"
+}
