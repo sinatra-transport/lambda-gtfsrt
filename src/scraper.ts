ae72f9167470e0e8061eb6f342d3f0278871da6e
Emily McLean <emilymcleandeveloper@gmail.com>
2025-05-18 14:48:20 +1000

    Remove got

--- src/scraper.ts
+++ src/scraper.ts
@@ -2,14 +2,37 @@ import { got } from 'got';
 import { transit_realtime } from './proto/gtfs-rt.js'
 import { trip_index } from './proto/trip-index.js';
 import { S3Client, S3ClientConfig, GetObjectCommand, GetObjectRequest } from '@aws-sdk/client-s3';
+import { request } from 'https';
+import { IncomingMessage } from 'http';
 
 export class Scraper {
 
+    _fetchAsUint8Array(url: string): Promise<Uint8Array> {
+        return new Promise((resolve, reject) => {
+            request(url, (res: IncomingMessage) => {
+                if (res.statusCode !== 200) {
+                    reject(new Error(`Request failed with status code ${res.statusCode}`));
+                    return;
+                }
+
+                const chunks: Buffer[] = [];
+
+                res.on('data', chunk => chunks.push(chunk));
+                res.on('end', () => {
+                    const buffer = Buffer.concat(chunks);
+                    resolve(new Uint8Array(buffer));
+                });
+            })
+                .on('error', reject)
+                .end();
+        });
+    }
+
     async gtfsRt(url: string): Promise<transit_realtime.FeedMessage> {
         console.log(`Downloading gtfs-rt from ${url}`);
-        const response = await got<Uint8Array>(url);
+        const response = await this._fetchAsUint8Array(url)
         console.log(`Successfully fetched gtfs-rt`);
-        return transit_realtime.FeedMessage.decode(response.rawBody);
+        return transit_realtime.FeedMessage.decode(response);
     }
 
     async tripIndex(key: string = "trip-index.pb"): Promise<trip_index.TripIndex> {
