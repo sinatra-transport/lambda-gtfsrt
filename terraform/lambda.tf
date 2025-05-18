c5db7b1a82de87caaad88030603fa6f1e4b992ce
Emily McLean <emilymcleandeveloper@gmail.com>
2025-05-18 13:19:21 +1000

    Redeploy lambda every time

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,14 @@
+resource "aws_s3_object_copy" "artifact" {
+  bucket             = data.terraform_remote_state.main_infra.outputs.bucket_lambda_name
+  key                = "${timestamp()}/lambda-gtfsrt.zip"
+  source             = "${data.terraform_remote_state.main_infra.outputs.bucket_lambda_name}/lambda-gtfsrt.zip"
+  checksum_algorithm = "SHA256"
+}
+
 resource "aws_lambda_function" "lambda_gtfsrt" {
     function_name = "gtfsrt"
-    s3_bucket = data.terraform_remote_state.main_infra.outputs.bucket_lambda_name
-    s3_key = "lambda-gtfsrt.zip"
+    s3_bucket = aws_s3_object_copy.artifact.bucket
+    s3_key = aws_s3_object_copy.artifact.key
     role = aws_iam_role.lambda_role.arn
     handler = "index.handler"
     runtime = "nodejs22.x"
@@ -11,6 +18,6 @@ resource "aws_lambda_function" "lambda_gtfsrt" {
 
     depends_on = [
         aws_iam_role_policy_attachment.lambda_logs,
-        aws_cloudwatch_log_group.cloudwatch_gtfsrt,
+        aws_cloudwatch_log_group.cloudwatch_gtfsrt
     ]
 }
