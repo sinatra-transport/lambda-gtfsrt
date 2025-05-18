fd0d9df306f0b70922b57ce9d23cc2e85b825be9
Emily McLean <emilymcleandeveloper@gmail.com>
2025-05-18 12:45:52 +1000

    Upload to S3 before deploying

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,10 +1,16 @@
-resource "aws_s3_object" "lambda_function_object" {
-  bucket  = data.terraform_remote_state.main_infra.outputs.bucket_lambda_arn
-  key     = "lambda-gtfsrt.zip"
-  content = data.local_file.lambda_function_archive.content
-  etag    = data.local_file.lambda_function_archive.content_md5
-}
+resource "aws_lambda_function" "lambda_gtfsrt" {
+    function_name = "gtfsrt"
+    s3_bucket = data.terraform_remote_state.main_infra.outputs.bucket_lambda_arn
+    s3_key = "lambda-gtfsrt.zip"
+    role = aws_iam_role.lambda_role
+    handler = "index.handler"
+    runtime = "nodejs22.x"
 
-data "local_file" "lambda_function_archive" {
-  filename = "${path.module}/lambda-gtfsrt.zip"
+    memory_size = 128
+    architectures = [ "x86_64" ]
+
+    depends_on = [
+        aws_iam_role_policy_attachment.lambda_logs,
+        aws_cloudwatch_log_group.cloudwatch_gtfsrt,
+    ]
 }
