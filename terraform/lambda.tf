a5c5a64fbe7dce9b270dda085ede71d37667a645
Emily McLean <emilymcleandeveloper@gmail.com>
2025-06-26 17:34:47 +1000

    Disable lambdas until new API portal is running

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -28,7 +28,7 @@ resource "aws_lambda_function" "lambda_gtfsrt" {
 resource "aws_cloudwatch_event_rule" "lambda_schedule_develop" {
   name = "lambda_gtfsrt_schedule_develop"
   description = "trigger lambda gtfsrt every 10 minutes for develop"
-  state = "ENABLED"
+  state = "DISABLED"
 
   schedule_expression = "rate(10 minutes)"
 }
@@ -60,7 +60,7 @@ resource "aws_lambda_permission" "allow_eventbridge_develop" {
 resource "aws_cloudwatch_event_rule" "lambda_schedule_prod" {
   name = "lambda_gtfsrt_schedule_prod"
   description = "trigger lambda gtfsrt every 2 minute for prod"
-  state = "ENABLED"
+  state = "DISABLED"
 
   schedule_expression = "rate(2 minutes)"
 }
